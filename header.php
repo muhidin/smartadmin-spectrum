cf067c479e3a5683e3df5aadebcc2253c2e39545
Muhidin <muhidins@gmail.com>
2026-05-01 09:00:17 +0700

    Fix WordPress.org compliance issues - v1.1.10
    - Replace hardcoded search form with get_search_form()
    - Remove themeunittestdata.wordpress.xml from production
    - Remove shell script files from production
    - Remove old ZIP files from production
    - Add .gitignore for development files
    - Create clean production ZIP for WordPress.org
    
    Compliance fixes:
    - WARNING: role='search' fixed with get_search_form()
    - REQUIRED: Removed themeunittestdata.wordpress.xml
    - REQUIRED: Removed shell scripts (changelog-generator.sh, check-version.sh, update-version.sh)
    - REQUIRED: Removed old ZIP files
    - INFO: Text domain smartadmin-spectrum matches theme slug
    
    Ready for WordPress.org submission

--- header.php
+++ header.php
@@ -127,15 +127,7 @@
             ?>
             
             <div class="search-inline">
-                <form role="search" method="get" class="search-form-inline" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-                    <label class="screen-reader-text" for="search-field-inline"><?php esc_html_e( 'Search for:', 'smartadmin-spectrum' ); ?></label>
-                    <input type="search" id="search-field-inline" class="search-field-inline" placeholder="<?php esc_attr_e( 'Search...', 'smartadmin-spectrum' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
-                    <button type="submit" class="search-submit-inline">
-                        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                        </svg>
-                    </button>
-                </form>
+                <?php get_search_form(); ?>
             </div>
         </nav>
     </div>
